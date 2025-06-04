b6a3ffeeb1ef3ecc72681dfb974f8504ef4f1fc6
FaceSDK <FaceAISDK.Service@gmail.com>
2025-06-04 10:14:35 +0800

    基础iOS 的封装

---
+++ FaceAISDK_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceAISDK_iOS
+//
+//  Created by anylife on 2025/6/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceAISDK_iOS/FaceAISDK_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAISDK_iOSApp.swift
+//  FaceAISDK_iOS
+//
+//  Created by anylife on 2025/6/4.
+//
+
+import SwiftUI
+
+@main
+struct FaceAISDK_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
