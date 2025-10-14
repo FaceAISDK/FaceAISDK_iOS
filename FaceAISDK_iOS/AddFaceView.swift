6b6fa65a2dd258aab778fb61cd768e223da121ef
FaceSDK <FaceAISDK.Service@gmail.com>
2025-10-14 16:49:57 +0800

    swiftUI 更新

--- FaceAISDK_iOS/AddFaceView.swift
+++ FaceAISDK_iOS/AddFaceView.swift
@@ -2,8 +2,7 @@ import SwiftUI
 import AVFoundation
 import FaceAISDK_Core
 
-
-let cameraSize: CGFloat = 300 //相机的尺寸
+let cameraSize: CGFloat = 320 //  相机的尺寸
 
 
 /**
@@ -21,36 +20,36 @@ public struct AddFaceView: View {
     public var body: some View {
         VStack {
             Text(viewModel.addFaceTips)
-                .font(.system(size: 19).bold())
+                .font(.system(size: 22).bold())
                 .padding(.bottom, 5)
                 .foregroundColor(.black)
             
             Text(viewModel.addFaceTipsExtra)
-                .font(.system(size: 17).bold())
+                .font(.system(size: 19).bold())
                 .padding(.bottom, 6)
                 .frame(minHeight: 30)
                 .foregroundColor(.black)
             
-            FaceAICameraView(session: viewModel.captureSession,cameraSize: cameraSize)
+            FaceAICameraView(session: viewModel.captureSession, cameraSize: cameraSize)
                 .frame(
-                    width: min(UIScreen.main.bounds.width,cameraSize),
-                    height: min(UIScreen.main.bounds.width,cameraSize))
-                .aspectRatio(1.0, contentMode: .fit) //Enforce1:1ratio
-                .clipShape(Circle()) // Clip to ensure square bounds
+                    width: min(UIScreen.main.bounds.width, cameraSize),
+                    height: min(UIScreen.main.bounds.width, cameraSize)
+                )
+                .aspectRatio(1.0, contentMode: .fit)
+                .clipShape(Circle())
                 .background(Color.white)
             
             Spacer()
         }
-        
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // 确保填满可用空间
+        .background(Color.white.ignoresSafeArea()) // 扩展到安全区域
         .overlay {
             if viewModel.readyConfirmFace {
-                // 弹窗让用户确认录入的人脸是否合规
                 PopupConfirmView(
-                    viewModel:viewModel,
+                    viewModel: viewModel,
                     onConfirm: {
-                        //保存人脸图和命名
-                        let facePath=viewModel.confirmSaveFaceAir(fileName: faceID)
-
+                        let facePath = viewModel.confirmSaveFaceAir(fileName: faceID)
                         onDismiss(facePath)
                     }
                 )
@@ -60,12 +59,11 @@ public struct AddFaceView: View {
         .onAppear {
             viewModel.initAddFace()
         }
-        .onDisappear{
+        .onDisappear {
             viewModel.stopAddFace()
         }
-        .padding()
-        .background(Color.white)
     }
+    
     
     
     struct PopupConfirmView: View {
@@ -75,22 +73,24 @@ public struct AddFaceView: View {
         var body: some View {
             VStack(alignment: .center) {
                 Text("人脸录入确认")
-                    .font(.headline)
-                    .foregroundColor(.black)
+                    .font(.system(size: 19).bold())
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                    .foregroundColor(.faceMain)
                     .padding()
                 
-                Text("请录入清晰无遮挡正脸图")
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.black)
-                    .font(.system(size: 14).bold())
-                
-                Image(uiImage:viewModel.canAddFace)
+                Image(uiImage: viewModel.canAddFace)
                     .resizable()
-                    .scaledToFit()
-                    .frame(width: 160)
-                    .cornerRadius(9)
+                    .scaledToFill()
+                    .frame(width: 140, height: 140)
+                    .cornerRadius(8)
                 
-                HStack(spacing: 30) {
+                Text("请录入无遮挡正脸清晰图")
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.faceMain)
+                    .padding(.vertical, 3)
+                    .font(.system(size: 15).bold())
+                
+                HStack(spacing: 10) {
                     Button("重试") {
                         viewModel.reInit()
                     }
@@ -108,9 +108,10 @@ public struct AddFaceView: View {
                     .foregroundColor(.white)
                     .cornerRadius(8)
                 }.padding()
-                
             }
-            .frame(maxWidth: .infinity, minHeight: 250)
+            .padding(.horizontal, 8)
+            .padding(.vertical, 4)
+            .frame(maxWidth: UIScreen.main.bounds.width-50, minHeight: 250)
             .background(Color.white)
             .cornerRadius(9)
             .shadow(radius: 9)
