3bf0c5d85ec570f5fc429616682031b9921b50fb
FaceSDK <FaceAISDK.Service@gmail.com>
2026-01-29 11:06:04 +0800

    人脸图Base64处理保存（可选，非SDK必需）

--- FaceAISDK_iOS/LivenessDetectView.swift
+++ FaceAISDK_iOS/LivenessDetectView.swift
@@ -60,7 +60,7 @@ struct LivenessDetectView: View {
                     .padding(.horizontal, 20)
                     .padding(.vertical, 9)
                     .foregroundColor(.white)
-                    .background(Color.brown)  
+                    .background(Color.faceMain)  
                     .cornerRadius(20)
                 
                 Text(localizedTip(for: viewModel.sdkInterfaceTipsExtra.code))
@@ -128,7 +128,7 @@ struct LivenessDetectView: View {
                                 .foregroundColor(.white)
                                 .frame(maxWidth: .infinity)
                                 .padding(.vertical, 10)
-                                .background(Color.brown)
+                                .background(Color.faceMain)
                                 .cornerRadius(10)
                         }
                         .padding(.horizontal, 30)
